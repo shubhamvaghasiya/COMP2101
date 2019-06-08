81e10cb3de0ea5fb195764c0cebf72ab570cadde
shubhamvaghasiya <skvaghasiya9@gmail.com>
2019-06-08 07:52:56 -0700

    lab1

--- bash/findsetidfiles.sh
+++ bash/findsetidfiles.sh
@@ -10,13 +10,17 @@ find / -type f -executable -perm -4000 -ls 2>/dev/null | sort -k 3
 # The second listing should display after the setuid file list, and be formatted similar to the setuid file list
 echo "setgid files:"
 echo "============="
+# type f : regular files , permission : 2000 , sort -k : sort by key (3 column)
 find / -type f -executable -perm -2000 -ls 2>/dev/null | sort -k 3
 # Task 2 for the exercise is to modify it to also display the 10 largest files in the system, sorted by their sizes
 
-echo "Largest files:"
+echo "find 10 Largest files:"
 echo "============="
 
 # The listing should include the file name, owner, and size in MBytes and be displayed after the listings of setuid and setgid files
-#ls: list the file content, l: file info rwx size etc
+
+# "MY COMMENTS"
+# ls: list the file content, l: file info rwx size etc a : dont remove entries with ".", h : human readable
+# sort r : reverse comparisons
 
 find /home/ -type f -exec ls -alh --block-size=M {} \; | sort -hr -k5 | head -n 10 | awk '{print $5, $3, $9}'
